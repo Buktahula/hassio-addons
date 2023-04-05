f52a998d7aff03145beb4b61bb307ce1ffafa7de
Buktahula <54060176+Buktahula@users.noreply.github.com>
2023-04-05 12:59:02 +0200

    Update run.sh

--- LG_ESS/data/run.sh
+++ LG_ESS/data/run.sh
@@ -5,12 +5,12 @@ CONFIG="/etc/essmqtt.conf"
 
 bashio::log.info "Initialize the essmqtt configuration..."
 
-echo "ess_password=$(bashio::config "options.ess_password")" > $CONFIG
-echo "mqtt_server=$(bashio::config "options.mqtt_server")" >> $CONFIG
-echo "mqtt_user=$(bashio::config "options.mqtt_user")" >> $CONFIG
-echo "mqtt_password=$(bashio::config "options.mqtt_password")" >> $CONFIG
-echo "interval_seconds=$(bashio::config "options.interval_seconds")" >> $CONFIG
-echo "hass_autoconfig_sensors=$(bashio::config "options.hass_autoconfig_sensors")" >> $CONFIG
+echo "ess_password=$(bashio::config "ess_password")" > $CONFIG
+echo "mqtt_server=$(bashio::config "mqtt_server")" >> $CONFIG
+echo "mqtt_user=$(bashio::config "mqtt_user")" >> $CONFIG
+echo "mqtt_password=$(bashio::config "mqtt_password")" >> $CONFIG
+echo "interval_seconds=$(bashio::config "interval_seconds")" >> $CONFIG
+echo "hass_autoconfig_sensors=$(bashio::config "hass_autoconfig_sensors")" >> $CONFIG
 
 #/usr/local/bin/essmqtt --config_file /etc/essmqtt.conf
  /bin/sh
